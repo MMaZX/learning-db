09692fdc8ca3669df33abfe0dbdf2220f4709998
fulanito <setup200yan@gmail.com>
2026-09-23 01:27:20 -0500

    add support certificate espectral and permissions

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
-    host: '127.0.0.1',
+    host: true, // escucha en 0.0.0.0, accesible desde tu IP local en la LAN
     port: 5173,
     proxy: {
       '/api': {
